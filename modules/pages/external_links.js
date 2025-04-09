98889a9e645ade1c24923133cf0e4b4e1f6833f7
Thomas Rasor <t.rasor@arrowlandgroup.com>
2025-04-09 14:04:01 -0500

    +changed external link icon paths to work on live deployment

--- modules/pages/external_links.js
+++ modules/pages/external_links.js
@@ -25,12 +25,12 @@ export class PageExternalLinks extends PageBase
 			e_btn.addEventListener('click', _ => { window.open(link, '_blank') });
 			if (icon && icon.length > 0) addElement(e_btn, 'img', '', (!label || label == '') ? style_icon_full : style_icon, _ => { _.src = icon });
 		};
-		AddButton('AURA GITHUB', 'https://github.com/ALGTRasor/AURA', '/resources/images/thirdparty/favicon_github.svg');
-		AddButton('ARROW LAND GROUP', 'https://www.arrowlandgroup.com/', '/resources/images/alg-favicon-32x32.png');
-		AddButton('AURA POWERAPP', 'https://apps.powerapps.com/play/e/default-af0df1fe-2a14-4718-8660-84733b9b72bc/a/01db799c-da90-49c5-a69e-c4d12d8375f0', '/resources/images/thirdparty/favicon_powerapps.ico');
-		AddButton('OUTLOOK', 'https://outlook.office.com/mail/', '/resources/images/thirdparty/favicon_outlook.ico');
-		AddButton('TEAMS', 'https://teams.microsoft.com/v2/', '/resources/images/thirdparty/favicon_teams.ico');
-		AddButton('GOOGLE MAPS', 'https://www.google.com/maps', '/resources/images/thirdparty/favicon_google_maps.ico');
+		AddButton('AURA GITHUB', 'https://github.com/ALGTRasor/AURA', 'resources/images/thirdparty/favicon_github.svg');
+		AddButton('ARROW LAND GROUP', 'https://www.arrowlandgroup.com/', 'resources/images/alg-favicon-32x32.png');
+		AddButton('AURA POWERAPP', 'https://apps.powerapps.com/play/e/default-af0df1fe-2a14-4718-8660-84733b9b72bc/a/01db799c-da90-49c5-a69e-c4d12d8375f0', 'resources/images/thirdparty/favicon_powerapps.ico');
+		AddButton('OUTLOOK', 'https://outlook.office.com/mail/', 'resources/images/thirdparty/favicon_outlook.ico');
+		AddButton('TEAMS', 'https://teams.microsoft.com/v2/', 'resources/images/thirdparty/favicon_teams.ico');
+		AddButton('GOOGLE MAPS', 'https://www.google.com/maps', 'resources/images/thirdparty/favicon_google_maps.ico');
 
 		this.e_cookie_warning = addElement(
 			e_body_root, 'div', '', 'pointer-events:none;position:absolute;left:0.25rem;right:0.25rem;bottom:0.25rem;height:3.25rem;font-size:0.65rem;text-align:center;opacity:50%;',
