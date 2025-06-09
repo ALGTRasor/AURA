3213693ce4d841cbbf21f345e9370b09a147437b
Thomas Rasor <t.rasor@arrowlandgroup.com>
2025-06-09 10:50:45 -0500

    +implemented core UserAllocation data for the Time Keeper page

--- modules/pages/descriptors/timekeep.js
+++ modules/pages/descriptors/timekeep.js
@@ -25,8 +25,10 @@ export class PageTimekeep extends PageDescriptor
 					e, false, true, 'flex-direction:column; align-content:stretch;',
 					e =>
 					{
-						addElement(e, 'div', '', style_panel_title, e => { e.innerText = "Allocations" });
-						instance.e_allocations_root = instance.CreatePanel(e, true, false, 'display:flex; flex-direction:column; flex-grow:1.0; overflow-y:auto; padding:var(--gap-05); gap:var(--gap-025); justify-content:flex-start;');
+						addElement(e, 'div', '', style_panel_title, e => { e.innerText = "Active Allocations" });
+						instance.e_unused_root = instance.CreatePanel(e, true, false, 'display:flex; flex-direction:column; flex-grow:1.0; overflow-y:auto; padding:var(--gap-05); gap:var(--gap-025); justify-content:flex-start;');
+						addElement(e, 'div', '', style_panel_title, e => { e.innerText = "Past Allocations" });
+						instance.e_used_root = instance.CreatePanel(e, true, false, 'display:flex; flex-direction:column; flex-basis:fit-content; flex-grow:0.0; overflow-y:auto; padding:var(--gap-05); gap:var(--gap-025); justify-content:flex-start;');
 					}
 				);
 			}
@@ -44,54 +46,57 @@ export class PageTimekeep extends PageDescriptor
 			return;
 		}
 
-		instance.e_allocations_root.innerHTML = '';
+		instance.e_used_root.innerHTML = '';
+		instance.e_unused_root.innerHTML = '';
 
 		let records = window.SharedData.userAllocations.instance.data.filter(_ => _.user_id === UserAccountInfo.account_info.user_id);
+		let records_used = records.filter(_ => _.use_total >= _.allocation_max);
+		let records_unused = records.filter(_ => _.use_total < _.allocation_max);
 
-		records.forEach(
-			(x, i, a) =>
-			{
-				const style_smol = 'font-size:80%; padding-left:var(--gap-1);';
-				let sum_used = x.use_total;// x.use_history.reduce((sum, record) => sum + record.hours, 0);
+		const create_in = (x, e) =>
+		{
+			const style_smol = 'font-size:80%; padding-left:var(--gap-1);';
+			let sum_used = x.use_total;// x.use_history.reduce((sum, record) => sum + record.hours, 0);
 
-				let e_item = CreatePagePanel(
-					instance.e_allocations_root, false, false, 'display:flex; flex-direction:row; border-radius:var(--gap-05); flex-basis:fit-content; flex-grow:0.0; flex-shrink:0.0; padding:var(--gap-05);',
-					_ =>
-					{
-						addElement(_, 'div', undefined, 'align-content:center;', x.Title.toUpperCase());
+			let e_item = CreatePagePanel(
+				e, false, false, 'display:flex; flex-direction:row; border-radius:var(--gap-05); flex-basis:fit-content; flex-grow:0.0; flex-shrink:0.0; padding:var(--gap-05);',
+				_ =>
+				{
+					addElement(_, 'div', undefined, 'align-content:center;', x.Title.toUpperCase());
 
-						let fillbar = FillBar.Create(
-							_,
-							`${x.allocation_max - sum_used} hours remaining`,
-							sum_used / x.allocation_max,
+					let fillbar = FillBar.Create(
+						_,
+						`${Math.round(sum_used / x.allocation_max * 100)}% used`,
+						sum_used / x.allocation_max,
+						{
+							label_alt: `${x.allocation_max - sum_used} hours available`,
+							from_hue_deg: 35.0, to_hue_deg: 65.0,
+							style_full: _ => { _.style.border = 'solid 1px cyan'; },
+							style_overfull: _ => { _.style.border = 'solid 2px orange'; },
+							check_color: (c, fill) =>
 							{
-								label_alt: `${Math.round(sum_used / x.allocation_max * 100)}% used`,
-								from_hue_deg: 35.0,
-								to_hue_deg: 65.0,
-								style_full: _ => { _.style.border = 'solid 1px cyan'; },
-								style_overfull: _ => { _.style.border = 'solid 2px orange'; },
-								check_color: (c, fill) =>
-								{
-									if (fill > 1.0) c = '#f003';
-									else if (fill == 1.0) c = '#0ff3'
-									else if (fill > 0.9) c = '#0f03';
-									return c;
-								}
+								if (fill > 1.0) c = '#f003';
+								else if (fill == 1.0) c = '#0ff3'
+								else if (fill > 0.9) c = '#0f03';
+								return c;
 							}
-						);
-					}
-				);
+						}
+					);
+				}
+			);
 
-				MegaTips.RegisterSimple(
-					e_item,
-					[
-						x.Title.toUpperCase(),
-						`(((${sum_used}))) / (((${x.allocation_max}))) hours used`,
-						`[[[${x.allocation_max - sum_used}]]] hours remaining`,
-					].join('<br>')
-				);
-			}
-		);
+			MegaTips.RegisterSimple(
+				e_item,
+				[
+					x.Title.toUpperCase(),
+					`(((${sum_used}))) / (((${x.allocation_max}))) hours used`,
+					`[[[${x.allocation_max - sum_used}]]] hours available`,
+				].join('<br>')
+			);
+		};
+
+		records_used.forEach((x, i, a) => { create_in(x, instance.e_used_root) });
+		records_unused.forEach((x, i, a) => { create_in(x, instance.e_unused_root) });
 	}
 
 	OnOpen(instance)
